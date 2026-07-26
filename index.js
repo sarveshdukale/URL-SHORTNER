bec30695b0e9665bd85e3fabbc1c8f49906a0f71
Sarvesh Dukale <sarveshdukale2004@gmail.com>
2026-07-27 00:47:34 +0530

    Authentication implementation

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 const urlRoute = require("./routes/url.route");
+const authRoute = require("./routes/auth.route");
 
 app.use("/api/url", urlRoute);
 
-app.use("api/user")
+app.use("/api/user", authRoute);
 
 DBConnect();
 app.listen(PORT,()=> console.log(`Server running on ${PORT}`))
