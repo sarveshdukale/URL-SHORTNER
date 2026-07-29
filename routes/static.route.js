e1d88ad62a62092aa7de4e17b627d10c3bf9ffca
Sarvesh Dukale <sarveshdukale2004@gmail.com>
2026-07-29 23:43:17 +0530

    signup route define

--- routes/static.route.js
+++ routes/static.route.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const { renderHomePage ,renderLoginPage } = require("../controllers/static.controller")
+const { renderHomePage ,renderLoginPage ,renderSignupPage} = require("../controllers/static.controller")
 
 router.get("/", renderHomePage);
-router.get("/login",renderLoginPage)
+router.get("/login", renderLoginPage)
+router.get("/signup",renderSignupPage)
 
 module.exports = router;
