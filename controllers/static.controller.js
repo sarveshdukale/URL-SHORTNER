48d94d05d1bc915797c432b71741fd79ee5df8ba
Sarvesh Dukale <sarveshdukale2004@gmail.com>
2026-07-31 23:50:37 +0530

    Send user in static route

--- controllers/static.controller.js
+++ controllers/static.controller.js
@@ -1,5 +1,7 @@
+const User = require("../models/user.model");
+
 function renderHomePage(req, res) {
-    return res.render("index");
+    return res.render("index",User);
 }
 
 function renderLoginPage(req, res) {
