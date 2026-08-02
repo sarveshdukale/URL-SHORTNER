8d1f6cf73cbc42f8ba1c9a3466ff7629f5b73bb1
Sarvesh Dukale <sarveshdukale2004@gmail.com>
2026-08-02 23:42:04 +0530

    bug fixing

--- models/url.model.js
+++ models/url.model.js
@@ -10,11 +10,12 @@ const urlSchema = new mongoose.Schema({
         type:String,
         required: true
     },
-    visitHistory: [{
-        timestamp: {
-        type: Number
-        }
-    }],
+    visitHistory: [{ timestamp: { type: Number } }],
+    // visitHistory: [{
+    //     timestamp: {
+    //     type: Number
+    //     }
+    // }],
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref:"user"
